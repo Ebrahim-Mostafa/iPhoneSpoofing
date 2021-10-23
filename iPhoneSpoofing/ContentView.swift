352145abc2e48823204c4fed9b3d7f630aa19caa
Ebahim Mostafa <57369953+Ebrahim-Mostafa@users.noreply.github.com>
2021-10-23 19:36:34 +0200

    Egypt IP Spoofing

---
+++ iPhoneSpoofing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iPhoneSpoofing
+//
+//  Created by Ibrahim Mostafa on 18/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to iPhone,IP spoofing!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iPhoneSpoofing/iPhoneSpoofingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhoneSpoofingApp.swift
+//  iPhoneSpoofing
+//
+//  Created by Ibrahim Mostafa on 18/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct iPhoneSpoofingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
